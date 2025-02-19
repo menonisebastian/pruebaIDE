72ce59835b465f7d973d64fa88b7ab1515b23c32
menonisebastian <arturomenoni@gmail.com>
2025-02-19 17:57:28 +0100

    segundo commit 002
    comentarios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.print("Fuaaaaaaaaaaaaaaaaaaaa!");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++)
